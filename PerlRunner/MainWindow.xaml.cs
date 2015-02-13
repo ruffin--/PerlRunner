7700f4eb3e38caffc2935029999ca85b4604222a
Ruffin Bailey <ruffin@rufwork.com>
2015-02-13 11:20:25 -0500

    Very mvp-style new file support.
    Could have done a nicer job (start with a new file each time? Alert the user to open or create? etc), but I think the next step is to remember open files. Once I commit to writing to disk somewhere the user essentially hasn't explicitly chosen/confirmed exists for saving which files were open for the last sesssion, I'll also make it so that you can run a new file without saving.

--- PerlRunner/MainWindow.xaml.cs
+++ PerlRunner/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace PerlRunner
     public partial class MainWindow : Window
     {
         string _wherePerl = string.Empty;
+        const string cstrNewFile = "New file";
 
         TabItem _tabActive
         {
@@ -64,9 +65,18 @@ namespace PerlRunner
 
             try
             {
-                if (!_tabActive.Name.Equals("FauxTab"))
+                bool bSaved = true;
+                if (_tabActive.Header.Equals(cstrNewFile))
+                {
+                    bSaved = _saveAsOrSaveNew();
+                }
+                else
                 {
                     _saveBuffer(_tabActive);
+                }
+
+                if (bSaved)
+                {
                     string argsForPerl = _activeFileLoc;
 
                     // Kludgey options from menus setup.
@@ -82,6 +92,10 @@ namespace PerlRunner
 
                     this.txtOutput.Text = CmdHelper.Execute(_wherePerl, argsForPerl);
                 }
+                else
+                {
+                    this.txtOutput.Text = "Code not executed because new file is unsaved to disk.\nHopefully not an issue in a later version.";
+                }
             }
             catch (Exception ex)
             {
@@ -92,6 +106,11 @@ namespace PerlRunner
         private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = true;    // Man, this is wonkily scoped. Fires only when within context. Don't think you can have it true from start.
+        }
+
+        private void CommandBinding_Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
 
         private void CommandBinding_Open_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -116,128 +135,77 @@ namespace PerlRunner
                     string openFilePath = dlg.FileName;
                     string openFileName = dlg.FileName.Substring(dlg.FileName.LastIndexOf(@"\") + 1);
 
-                    if (dFileLocs.ContainsKey(openFileName))
-                    {
-                        this.txtOutput.Text = "A file named " + openFileName + " is already open. Please \n"
-                            + "close the existing tab by this name to open this file.\n"
-                            + "\n"
-                            + "With any luck, this won't be a limitation in the future.\n"
-                            + "\n"
-                            + "\t(It is on github, and pull requests are welcome, you know. ;^D)";
-                        MessageBox.Show("Not gonna open that right now.\n\n(see bottom window)", "Not gonna do it.");
-                    }
-                    else
-                    {
-                        this.dFileLocs.Add(openFileName, openFilePath);
-
-                        TextBoxTabsToSpaces txt = new TextBoxTabsToSpaces();
-                        txt.FontFamily = new FontFamily("Courier New");
-                        txt.AcceptsReturn = true;
-                        txt.AcceptsTab = true;
-                        txt.Background = Brushes.LightBlue;
-                        txt.Text = File.ReadAllText(openFilePath);
-                        txt.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
-                        txt.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
-
-                        TabItem tab = new TabItem();
-                        tab.Header = openFileName;
-                        tab.Content = txt;
-                        this.TabOFiles.Items.Insert(this.TabOFiles.Items.Count, tab);
-                        this.TabOFiles.SelectedItem = tab;
-                        CommandManager.InvalidateRequerySuggested();
-
-                        tab.Focus();
-                        UpdateLayout();
-                        txt.Focus();
-
-                        this.txtOutput.Text = openFileName + " opened successfully.";
-                    }
+                    _createTab(openFileName, openFilePath);
                 }
             }
             catch (Exception ex)
             {
                 this.txtOutput.Text = ("Unable to open file.\n\n" + ex.ToString());
             }
-
         }
 
         private void CommandBinding_Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = !string.IsNullOrWhiteSpace(_txtActive.Text) && !_tabActive.Header.Equals("Open a File");
+            e.CanExecute = this.TabOFiles.Items.Count > 0;
         }
 
         private void CommandBinding_Save_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            if (!_tabActive.Header.Equals("Open a File"))
+            if (_tabActive.Header.Equals(cstrNewFile))
+                _saveAsOrSaveNew();
+            else
                 _saveBuffer(_tabActive);
+        }
+
+        private void CommandBinding_New_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_New_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            _createTab(cstrNewFile);
         }
 
         private void CommandBinding_SaveAs_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = !_tabActive.Header.Equals("Open a File");
+            e.CanExecute = this.TabOFiles.Items.Count > 0;
         }
 
         private void CommandBinding_SaveAs_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             if (!_tabActive.Header.Equals("Open a File"))
-                _saveAsOrSaveNew(true);
+                _saveAsOrSaveNew();
         }
 
-        private void _saveAsOrSaveNew(bool saveAs = false)
+        private void CommandBinding_CloseTab_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
-            dlg.FileName = string.Empty;
-            dlg.DefaultExt = ".pl"; // Default file extension
-            dlg.Filter = "Perl scripts (.pl)|*.pl|All Files|*.*"; // Filter files by extension 
-
-            Nullable<bool> result = dlg.ShowDialog();
-
-            // Process open file dialog box results 
-            if (result.HasValue && result.Value)
-            {
-                // Open document
-                // NOTE: Never mix data with view like this, okay? LOOK AWAY!!! I'M HIDEOUS!!!
-                string saveFilePath = dlg.FileName;
-                string saveFileName = saveFilePath.Substring(saveFilePath.LastIndexOf(@"\") + 1);
-
-                if (dFileLocs.ContainsKey(saveFileName))
-                {
-                    this.txtOutput.Text = "A file named " + saveFileName + " is already open. Please \n"
-                        + "close the existing tab by this name to save this file with that name.\n"
-                        + "\n"
-                        + "With any luck, this won't be a limitation in the future.\n"
-                        + "\n"
-                        + "\t(This app is on github, and pull requests are welcome, you know. ;^D)";
-                    MessageBox.Show("Not gonna open that right now.\n\n(see bottom window)", "Not gonna do it.");
-                }
-                else
-                {
-                    if (saveAs)
-                    {
-                        dFileLocs.Remove(_tabActive.Header.ToString());
-                    }
-                    dFileLocs.Add(saveFileName, saveFilePath);
-                    _tabActive.Header = saveFileName;
-                    _saveBuffer(_tabActive);
-
-                    this.txtOutput.Text = saveFileName + " saved successfully.";
-                }
-            }
-        }
-
-        private void CommandBinding_Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
+            e.CanExecute = this.TabOFiles.Items.Count > 0;
         }
 
         private void CommandBinding_CloseTab_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             try
             {
-                if (!_tabActive.Name.Equals("FauxTab"))
+                if (this.TabOFiles.Items.Count > 0) // overly defensive. CanExecute should've stopped it already.
                 {
-                    _saveBuffer(_tabActive);
-                    this.txtOutput.Text = "Saving before closing."; // TODO: Check for unsaved edits instead.
+                    if (_tabActive.Header.Equals(cstrNewFile))
+                    {
+                        MessageBoxResult result = MessageBox.Show("Save new file before closing?", "Save new file?", 
+                            MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                        if (MessageBoxResult.Yes == result)
+                        {
+                            _saveBuffer(_tabActive);
+                            this.txtOutput.Text += "\nSaved and now closing tab.";
+                        }
+                    }
+                    else
+                    {
+                        _saveBuffer(_tabActive);
+                        this.txtOutput.Text = "Saving before closing."; // TODO: Check for unsaved edits instead.
+                    }
+
                     this.dFileLocs.Remove(_tabActive.Header.ToString());
                     this.TabOFiles.Items.Remove(_tabActive);
                 }
@@ -246,11 +214,6 @@ namespace PerlRunner
             {
                 this.txtOutput.Text += "Unable to close tab.\n\n" + ex.ToString();
             }
-        }
-
-        private void CommandBinding_CloseTab_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = true;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -280,6 +243,114 @@ namespace PerlRunner
 
             this.txtOutput.Text += "\nApp icon is part of the Creative Commons and requires attribution for the author: Nicolas Mollet.\n"
                 + "Thanks Nicolas.\n";
+        }
+
+        private void _createTab(string openFileName, string openFilePath = "")
+        {
+            if (dFileLocs.ContainsKey(openFileName))
+            {
+                string strTextWarning = string.Empty;
+                string strMsgBoxWarning = string.Empty;
+
+                if (openFileName.Equals(cstrNewFile))
+                {
+                    strTextWarning = "You already have a new file open. Please save\n"
+                        + "the existing new file before opening another.\n"
+                        + "\n"
+                        + "With any luck, this won't be a limitation in the future.\n"
+                        + "\n"
+                        + "\t(It is on github, and pull requests are welcome, you know. ;^D)";
+                    strMsgBoxWarning = "Save existing new file first, please.\n\n(see bottom window)";
+                }
+                else
+                {
+                    strTextWarning = "A file named " + openFileName + " is already open. Please close the\n"
+                        + "existing tab by this name to open this file.\n"
+                        + "\n"
+                        + "With any luck, this won't be a limitation in the future.\n"
+                        + "\n"
+                        + "\t(It is on github, and pull requests are welcome, you know. ;^D)";
+                    strMsgBoxWarning = "Not gonna open that right now.\n\n(see bottom window)";
+                }
+                this.txtOutput.Text = strTextWarning;
+                MessageBox.Show(strMsgBoxWarning, "Not gonna do it.");
+            }
+            else
+            {
+                this.dFileLocs.Add(openFileName, openFilePath);
+
+                TextBoxTabsToSpaces txt = new TextBoxTabsToSpaces();
+                txt.FontFamily = new FontFamily("Courier New");
+                txt.AcceptsReturn = true;
+                txt.AcceptsTab = true;
+                txt.Background = Brushes.LightBlue;
+                if (!string.IsNullOrEmpty(openFilePath)) txt.Text = File.ReadAllText(openFilePath);
+                txt.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                txt.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+
+                TabItem tab = new TabItem();
+                tab.Header = openFileName;
+                tab.Content = txt;
+                this.TabOFiles.Items.Insert(this.TabOFiles.Items.Count, tab);
+                this.TabOFiles.SelectedItem = tab;
+                CommandManager.InvalidateRequerySuggested();
+
+                tab.Focus();
+                UpdateLayout();
+                txt.Focus();
+
+                this.txtOutput.Text = openFileName.Equals(cstrNewFile) ? "New file created" : openFileName + " opened successfully.";
+            }
+        }
+
+        private bool _saveAsOrSaveNew()
+        {
+            bool bSavedSuccessfully = false;
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = string.Empty;
+            dlg.DefaultExt = ".pl"; // Default file extension
+            dlg.Filter = "Perl scripts (.pl)|*.pl|All Files|*.*"; // Filter files by extension 
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result.HasValue && result.Value)
+            {
+                // Open document
+                // NOTE: Never mix data with view like this, okay? LOOK AWAY!!! I'M HIDEOUS!!!
+                string saveFilePath = dlg.FileName;
+                string saveFileName = saveFilePath.Substring(saveFilePath.LastIndexOf(@"\") + 1);
+
+                if (saveFileName.Equals(cstrNewFile))
+                {
+                    string strMsg = @"No, you can't call your saved file ""New file""\n"
+                        + "without an extension, smart-aleck. ;^)";
+                    this.txtOutput.Text = strMsg;
+                    MessageBox.Show(strMsg, "I see what you did there.");
+                }
+                else if (dFileLocs.ContainsKey(saveFileName))
+                {
+                    this.txtOutput.Text = "A file named " + saveFileName + " is already open. Please \n"
+                        + "close the existing tab by this name to save this file with that name.\n"
+                        + "\n"
+                        + "With any luck, this won't be a limitation in the future.\n"
+                        + "\n"
+                        + "\t(This app is on github, and pull requests are welcome, you know. ;^D)";
+                    MessageBox.Show("Not gonna open that right now.\n\n(see bottom window)", "Not gonna do it.");
+                }
+                else
+                {
+                    dFileLocs.Remove(_tabActive.Header.ToString());
+                    dFileLocs.Add(saveFileName, saveFilePath);
+                    _tabActive.Header = saveFileName;
+                    _saveBuffer(_tabActive);
+                    bSavedSuccessfully = true;
+
+                    this.txtOutput.Text = saveFileName + " saved successfully.";
+                }
+            }
+
+            return bSavedSuccessfully;
         }
 
         private string _fileForTab(TabItem tab)
